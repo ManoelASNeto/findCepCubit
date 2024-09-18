606b9ebfba08d8573a5a1b97c16a59d60fd3a929
Manoel Amâncio da Silva Neto <manoelamanciodasilvaneto@Manoels-MacBook-Pro.local>
2024-09-17 23:35:37 -0300

    change page and bloc provider

--- lib/features/find_cep/presentation/pages/cep_search_page.dart
+++ lib/features/find_cep/presentation/pages/cep_search_page.dart
@@ -1,69 +1,117 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:via_cep_cubit/features/find_cep/presentation/cubit/find_cep_cubit.dart';
 
 class CepSearchPage extends StatelessWidget {
   final cepEC = TextEditingController();
-  CepSearchPage({super.key});
+  final FindCepCubit findCepCubit;
+  final _formKey = GlobalKey<FormState>();
+  CepSearchPage({super.key, required this.findCepCubit});
+
+  String? validateCEP(String? cep) {
+    if (cep == null || cep.isEmpty || cep.length < 8) {
+      return 'Digite um CEP válido';
+    }
+
+    String pattern = r'^\d{5}-?\d{3}$';
+    RegExp regex = RegExp(pattern);
+
+    if (!regex.hasMatch(cep)) {
+      return 'CEP inválido';
+    }
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        height: size.height,
-        width: size.width,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/background.png'),
-            fit: BoxFit.cover,
-            opacity: 0.4,
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: Container(
+          height: size.height,
+          width: size.width,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                  'https://img.freepik.com/vetores-gratis/ilustracao-da-pesquisa-mundial_53876-5870.jpg?t=st=1726623795~exp=1726627395~hmac=7da819674ee7310dc1611cee1796a6c89e535129fb8f209c159c8bd984e64637&w=740'),
+              fit: BoxFit.cover,
+              opacity: 0.4,
+            ),
           ),
-        ),
-        child: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 70),
-              child: Text(
-                'Buscador de CEP',
-                style: TextStyle(
-                  fontFamily: 'SofadiOne',
-                  fontSize: 30,
-                ),
-              ),
-            ),
-            const Text(
-              'Insira um cep do territorio Brasileiro para obter informações',
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Digite o CEP',
-                  isDense: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(20),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 70),
+                  child: Text(
+                    'Buscador de CEP',
+                    style: TextStyle(
+                      fontFamily: 'SofadiOne',
+                      fontSize: 30,
                     ),
                   ),
                 ),
-                keyboardType: TextInputType.number,
-              ),
+                const Text(
+                  'Insira um cep do território Brasileiro para obter informações',
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: TextFormField(
+                    validator: validateCEP,
+                    controller: cepEC,
+                    decoration: const InputDecoration(
+                      labelText: 'Digite o CEP',
+                      isDense: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                      ),
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      findCepCubit.getCep(cepEC.text);
+                      FocusManager.instance.primaryFocus?.unfocus();
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    elevation: 10,
+                  ),
+                  child: const Text('Pesquisar'),
+                ),
+                const Spacer(),
+                SizedBox(
+                  height: 200,
+                  child: BlocBuilder<FindCepCubit, FindCepState>(builder: (context, state) {
+                    if (state is FindCepLoaded) {
+                      return Column(
+                        children: [
+                          Text('O Cep pesquisado foi ${state.findCepEntity.cep ?? 'Não encontrado'}'),
+                          Text('Logradouro: ${state.findCepEntity.logradouro}'),
+                          Text('Bairro: ${state.findCepEntity.bairro}'),
+                          Text('Localidade: ${state.findCepEntity.localidade}'),
+                        ],
+                      );
+                    }
+                    return const SizedBox();
+                  }),
+                )
+              ],
             ),
-            const SizedBox(
-              height: 15,
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                elevation: 10,
-              ),
-              child: const Text('Pesquisar'),
-            ),
-          ],
+          ),
         ),
       ),
       // body: BlocBuilder<FindCepCubit, FindCepState>(
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:via_cep_cubit/di.dart';
+import 'package:via_cep_cubit/features/find_cep/presentation/cubit/find_cep_cubit.dart';
 import 'features/find_cep/presentation/pages/cep_search_page.dart';
 import 'di.dart' as di;
 
@@ -12,12 +15,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final cubit = di.sl<FindCepCubit>();
     return MaterialApp(
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: CepSearchPage(),
+      home: BlocProvider(
+        create: (context) => cubit,
+        child: CepSearchPage(
+          findCepCubit: cubit,
+        ),
+      ),
     );
   }
 }
